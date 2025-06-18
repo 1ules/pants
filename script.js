80affcf31ba7fa3369ac1e9aff5d0ac966d55989
1ules <58918675+1ules@users.noreply.github.com>
2025-06-18 16:42:33 +1000

    Update script.js

--- script.js
+++ script.js
@@ -649,6 +649,7 @@ function endGame() {
     
     document.getElementById('gameArea').style.display = 'none';
     document.getElementById('resultsArea').style.display = 'block';
+    document.getElementById('homeBtn').style.display = 'none';
     showResults();
     displayStats();
 }
